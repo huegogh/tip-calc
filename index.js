4be434f6c69828bd3894718c1b9c108a9ee4001a
huegogh <huegogh42@gmail.com>
2021-05-10 10:45:43 -0700

    add total calculations

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ let percentageDisplay = document.getElementById('sliderValue');
 let tipTotal = document.getElementById('tipTotal');
 let grandTotal = document.getElementById('grandTotal');
 
-total.innerText = 20.00;
 
-slider.addEventListener( 'input', () => {
-    percentageDisplay.innerText = slider.value;
-    grandTotal.innerText = (slider.value * total.value) / 100.00;
-})
+slider.addEventListener('input', updateTotals);
+total.addEventListener('input', updateTotals);
+
+function updateTotals() {
+    percentageDisplay.innerText = slider.value + '%';
+    tipTotal.innerText = `Tip: $${((slider.value * parseFloat(total.value)) / 100).toFixed(2)}`;
+    grandTotal.innerText = `Grand Total: $${(parseFloat(total.value) + (slider.value * parseFloat(total.value) / 100)).toFixed(2)}`;
+
+}
+
+updateTotals();
